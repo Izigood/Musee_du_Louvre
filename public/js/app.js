6cf7e74583b5eb28d58b59dc1e8f21d7d294384b
David <lartigue.david@gmail.com>
2019-04-27 17:35:42 +0200

    Modification du fichier app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,56 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+var stripe = Stripe('pk_test_MzbipRiIwdr2hPt0YkpvtalI00yamvyd8q');
+var elements = stripe.elements();
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+// Custom styling can be passed to options when creating an Element.
+var style = {
+    base: {
+        // Add your base input styles here. For example:
+        fontSize: '16px',
+        color: "#32325d",
+    }
+};
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+        displayError.textContent = event.error.message;
+    } else {
+        displayError.textContent = "Veuillez saisir les informations de votre carte bancaire !";
+    }
+});
+
+// Create a token or display an error when the form is submitted.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    stripe.createToken(card).then(function(result) {
+        if (result.error) {
+        // Inform the customer that there was an error.
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+        } else {
+        // Send the token to your server.
+        stripeTokenHandler(result.token);
+        }
+    });
+});
+
+function stripeTokenHandler(token) {
+    // Insert the token ID into the form so it gets submitted to the server
+    var form = document.getElementById('payment-form');
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+
+    // Submit the form
+    form.submit();
+}
